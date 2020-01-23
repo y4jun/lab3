1e9a08a83eb0950dab57745380e3a7e939d69999
Yongho Jun <y4jun@ucsd.edu>
2020-01-23 10:30:23 -0800

    lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,30 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+        $("#testjs").text("Please wait...");
+        $(".jumbotron p").addClass("active");
+        $(".jumbotron p").toggleClass("active");
 	});
+    
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
+
+$('#myelement').click(changeText);
+function changeText(event) {
+    $(this).text("Changed text");
+}
+
+$("a.thumbnnail").click(projectClick);
+
+$('#testjs').click(function(){
+});
+//var containingProject = $(this).closest(".project");
+//    var description = $(containingProject).find(".project-description");
+//    if (description.length == 0) {
+//       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+//    } else {
+//       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+//    }
